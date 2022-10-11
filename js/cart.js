33445cf62feda057475abe926fc5ea5f02c83ff0
agusraimundez <agusr27@hotmail.com>
2022-10-11 20:23:59 -0300

    Update E5 pt3 con error

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,7 @@
 let enlaceCarrito = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
-
-
 function mostrarProducto(array){
-
+ 
     let htmlContentToAppend = "";
-    let subTotal;
-    subTotal=(array.articles[0].unitCost)*(array.articles[0].count);
     htmlContentToAppend+=`
     <table class="table">
   <thead>
@@ -21,22 +17,30 @@ function mostrarProducto(array){
     <tr>
       <th scope="row"><img id="img_size" src="${array.articles[0].image}" style="width: 70px;height:fit-content;"></th>
       <td>${array.articles[0].name}</td>
-      <td>${array.articles[0].currency} ${array.articles[0].unitCost}</td>
-      <td><input type="number" id="cant_prod" value="${array.articles[0].count}"min="1"></td>
-      <td>${array.articles[0].currency} ${subTotal}</td>
+      <td>${array.articles[0].currency}<p id="costoProd"> ${array.articles[0].unitCost}</p></td>
+      <td><input type="number" id="cantProd" min="1" onchange="${updateValue(array.articles[0])}"></td>
+      <td>${array.articles[0].currency} <p id="subTotal"></p></td>
     </tr>
   </tbody>
-</table>`
+</table>
+`
 
 document.getElementById("infoProducto").innerHTML += htmlContentToAppend;
+
 }
 
+function updateValue(array) {
+  let cant = 2;
+  array.count = cant;
+  let subtotal = array.count * array.unitCost;
+ document.getElementById("subTotal").innerHTML = subtotal;
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
 getJSONData(enlaceCarrito).then(function (resultObj) {
     if (resultObj.status === "ok") {
-        mostrarProducto(resultObj.data);
+      mostrarProducto(resultObj.data);
     }
 });
 
